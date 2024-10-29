081f1ce61562d024896b8792a7885b7b7bc53daf
thedamnandres <andres_jimenez666@hotmail.com>
2024-10-29 08:24:34 -0500

    Ultimos cambios en el codigo e imlementacion de boton con la alerta.

--- JimenezAndres_Examen_Pgr1/wwwroot/js/site.js
+++ JimenezAndres_Examen_Pgr1/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+document.getElementById("BotonInfo").addEventListener("click", function () {
+    alert("Mi nombre es Andres Jimenez y mi hobbie es escuchar musica.");
+});
